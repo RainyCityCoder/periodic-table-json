c1bc2d6e6e69c38063fb7552d8023a7564458a8b
rainycitycoder <cvoitmembership@proton.me>
2025-08-15 16:30:03 -0700

    Added check to ensure entered element isn't repeat, & type hints to funct params.

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ Description:
     periodic-table.json file: the user enters element information, then the 
     program builds the JSON objects and adds them to the file.
 Author: RainyCityCoder
-Special thanks: rndpkt, Saad, Carcigenicate on Discord.
+Special thanks: rndpkt, Saad, Carcigenicate on Discord, & cherimarie on GitHub. 
 '''
 
 import json
@@ -12,7 +12,7 @@ import json
 def read_data(filename):
     '''Reads data from JSON file into program
 
-    Args:
+    Parameters:
         filename (str): name of JSON file program will work with
 
     Returns:
@@ -30,10 +30,10 @@ def read_data(filename):
     return all_json_data
         
 
-def write_data(all_json_data, filename):
+def write_data(all_json_data: list, filename:str):
     '''Writes JSON data to JSON file
 
-    Args:
+    Parameters:
         all_json_data (list): list of JSON objects
         filename (str): name of JSON file program will work with
     '''
@@ -43,10 +43,13 @@ def write_data(all_json_data, filename):
         print("Written to file")
 
 
-def build_object(name, atomic_num, symbol, atomic_weight):
+def build_object(name: str, 
+                 atomic_num: int, 
+                 symbol: str, 
+                 atomic_weight: float):
     '''Builds dictionary from user input
 
-    Args:
+    Parameters:
         name (str): Name of element
         atomic_num (int): atomic number of element
         symbol (str): Symbol of element
@@ -61,36 +64,36 @@ def build_object(name, atomic_num, symbol, atomic_weight):
             "Atomic Weight": atomic_weight}
     
     
-def atomic_number_check(prompt = ''):
+def atomic_number_check(p: str = ''):
     '''Checks user-entered atomic number is integer
 
-    Args:
-        prompt (str, optional): user's entry. Defaults to ''.
+    Parameters:
+        p (str, optional): user's entry. Defaults to ''.
 
     Returns:
         int: atomic number of element
     '''
     
     try:
-        atomic_num = int(input(prompt))
+        atomic_num = int(input(p))
     except ValueError:
         print("Error: Input must be a valid integer.")
     else: 
         return atomic_num
     
     
-def atomic_weight_check(prompt = ''):
+def atomic_weight_check(p: str = ''):
     '''Checks user-entered atomic weight is float
 
-    Args:
-        prompt (str, optional): user's entry. Defaults to ''.
+    Parameters:
+        p (str, optional): user's entry. Defaults to ''.
 
     Returns:
         float: atomic weight of element
     '''
     
     try:
-        atomic_weight = input(prompt)
+        atomic_weight = input(p)
         atomic_weight = float(atomic_weight)
     except ValueError:
         print("Error: Input must be a valid number.")
@@ -98,36 +101,47 @@ def atomic_weight_check(prompt = ''):
         return atomic_weight
     
     
-def main_loop(json_data):
+def main_loop(json_data: list):
     '''Main program flow. Checks if user wants to continue, calls atomic number
     and atomic weight check functions. Calls build_object function to add user
     input to existing JSON data.
 
-    Args:
+    Parameters:
         json_data (list): list of JSON objects
+        
+    Return: json_data (list): updated list of JSON objects
     '''
     
     while True:
         continue_inputs = input("Continue? (y/n): ")
         if continue_inputs == "n": break
         if continue_inputs != "y": continue
-            
+             
         atomic_num = atomic_number_check("Atomic Number: ")
         symbol = input("Symbol: ")
         name = input("Name: ")
         atomic_weight = atomic_weight_check("Atomic weight: ")
         
         new_object = build_object(name, atomic_num, symbol, atomic_weight)
-        json_data.append(new_object)
+        
+        element_already_present = list(filter(
+            lambda i: i["Atomic Number"] == new_object["Atomic Number"], 
+            json_data))
+        if not element_already_present:
+            json_data.append(new_object)
+        if element_already_present:
+            print("Repeat element entered; ignoring.")
+        
+    return json_data
     
     
 def main():
     '''Calls main program functions
     '''
     all_json_data = read_data('periodic-table.json')
-    main_loop(all_json_data)
+    all_json_data = main_loop(all_json_data)
     write_data(all_json_data, 'periodic-table.json')
     
     
 if __name__ == '__main__':
-    main()    
+    main()
